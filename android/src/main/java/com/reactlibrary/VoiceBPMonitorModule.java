afac94e5a1e3708371817600222bd293301909aa
SivakumarJ <siva.jcn@gmail.com>
2018-07-24 14:24:10 +0530

    location permission added

--- android/src/main/java/com/reactlibrary/VoiceBPMonitorModule.java
+++ android/src/main/java/com/reactlibrary/VoiceBPMonitorModule.java
@@ -1,8 +1,16 @@
 package com.reactlibrary;
 
+
+import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothManager;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Handler;
+
 import com.cooey.devices.bpmeter.VoiceBpMeterCallBack;
 import com.cooey.devices.bpmeter.VoiceBpMeterControls;
+import com.facebook.react.bridge.ActivityEventListener;
 import com.facebook.react.bridge.Arguments;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
@@ -10,25 +18,46 @@ import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.WritableMap;
 
 
-public class VoiceBPMonitorModule extends ReactContextBaseJavaModule implements VoiceBpMeterCallBack {
+
+public class VoiceBPMonitorModule extends ReactContextBaseJavaModule implements VoiceBpMeterCallBack, ActivityEventListener {
 
     private final ReactApplicationContext reactContext;
     private VoiceBpMeterControls voiceBpMeterControls;
     private Boolean mConnected = false;
+    private BluetoothAdapter bluetoothAdapter;
+    private final static int REQUEST_ENABLE_BT = 1;
+
 
     public VoiceBPMonitorModule(ReactApplicationContext reactContext) {
         super(reactContext);
         this.reactContext = reactContext;
 
+        final BluetoothManager bluetoothManager =
+                (BluetoothManager) this.reactContext.getSystemService(Context.BLUETOOTH_SERVICE);
+        bluetoothAdapter = bluetoothManager.getAdapter();
     }
 
 
     @ReactMethod
     public void connectBPMonitor() {
+        this.startConnection();
+
+    }
+
+    private void  startConnection() {
         //Inititalize the voice bp meter with context and callback interface
-        this.voiceBpMeterControls = new VoiceBpMeterControls(this.reactContext, this);
-        //Enable scan of the voice bp meter
-        mConnected = this.voiceBpMeterControls.scanLeDevice(true);
+        if (bluetoothAdapter == null || !bluetoothAdapter.isEnabled()) {
+            Intent enableBtIntent =
+                    new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            final Activity activity = getCurrentActivity();
+            activity.startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+
+        }else {
+            this.voiceBpMeterControls = new VoiceBpMeterControls(this.reactContext, this);
+            //Enable scan of the voice bp meter
+            mConnected = this.voiceBpMeterControls.scanLeDevice(true);
+        }
+
 
     }
 
@@ -94,5 +123,24 @@ public class VoiceBPMonitorModule extends ReactContextBaseJavaModule implements 
     @Override
     public String getName() {
         return "VoiceBPMonitor";
+    }
+
+    @Override
+    public void onActivityResult(Activity activity, int requestCode, int resultCode, Intent data) {
+
+        if(requestCode == REQUEST_ENABLE_BT) {
+            if (resultCode == Activity.RESULT_CANCELED) {
+                WritableMap params = Arguments.createMap();
+                params.putString("error","bluetooth permission denied");
+                RNCooeyModule.sendEvent(this.reactContext,"voice_bp_error",params);
+            }else {
+                this.startConnection();
+            }
+        }
+    }
+
+    @Override
+    public void onNewIntent(Intent intent) {
+
     }
 }
--- cooeyExample/App.js
+++ cooeyExample/App.js
@@ -2,18 +2,38 @@ import React from 'react';
 import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 import { VoiceBPMonitor, CooeyEvents} from 'react-native-cooey';
 
-const eventsForCooey = CooeyEvents;
 const bpMonitor = VoiceBPMonitor;
 
 export default class App extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.eventListerner = CooeyEvents.addListener(
+      "voice_bp_connection",
+      this.connectionChange.bind(this)
+    );
+    this.state = {
+      connected:false
+    }
+  }
+
+  connectionChange(status) {
+    this.setState({connected:status});
+  }
   render() {
+    let title = this.state.connected ? "Take Reading" : "connect BP"
     return (
       <View style={styles.container}>
 
         <TouchableOpacity onPress={() => {
-          bpMonitor.connectBPMonitor();
+          if(this.state.connected) {
+            bpMonitor.takeReading()
+          }else {
+            bpMonitor.connectBPMonitor();
+          }
+      
         }}>
-          <Text style={{borderWidth:1, padding:10}}>{"connect BP"}</Text>
+          <Text style={{borderWidth:1, padding:10}}>{title}</Text>
         </TouchableOpacity>
       </View>
     );
