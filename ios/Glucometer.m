6c994aa19878706defda76b8d52bbcbfe0118b95
SivakumarJ <siva.jcn@gmail.com>
2018-11-09 13:22:40 +0530

    bug fixes

--- ios/Glucometer.m
+++ ios/Glucometer.m
@@ -54,7 +54,7 @@ RCT_EXPORT_METHOD(startMeasuring:(NSInteger)testType) {
     // 1 - fasting
     // 2 - after meal
     // 3 - random
-    
+    [self.bloodTester wakeupDevice];
     [self.bloodTester setTestType:testType];
 }
 
@@ -92,6 +92,7 @@ RCT_EXPORT_METHOD(stopMeasuring) {
     NotifyStatus *notifyStatus = (NotifyStatus*)notification.object;
     NSDictionary *params = notifyStatus.params;
     DNUTestStatus status = notifyStatus.status;
+    NSLog(@"GLUCODEVICE status %ld", (long)status);
     switch (status) {
         case TEST_STATUS_WAITING_DEVICE_PLUGIN: {
             [self sendEventWithName:@"gluco_device_connection" body:@{@"status":@"plugged_out"}];
@@ -99,28 +100,35 @@ RCT_EXPORT_METHOD(stopMeasuring) {
             break;
         case TEST_STATUS_WAKING_UP_DEVICE: {
             [self sendEventWithName:@"gluco_device_connection" body:@{@"status":@"communicating"}];
-        }break;
+        }
+            break;
         case TEST_STATUS_RECOGNIZE_DEVICE: {
             [self sendEventWithName:@"gluco_device_connection" body:@{@"status":@"recognized"}];
-        }break;
+        }
+            break;
         case TEST_STATUS_PAPER_INSERTED: {
             [self sendEventWithName:@"gluco_device_connection" body:@{@"status":@"readyToTest"}];
         }
+            break;
         case TEST_STATUS_DEVICE_CHECKE_FINISH: {
             [self sendEventWithName:@"gluco_device_connection" body:@{@"status":@"insert_paper"}];
-        }break;
+        }
+            break;
         case TEST_STATUS_PAPER_USED: {
             [self sendEventWithName:@"gluco_device_connection" body:@{@"status":@"old_paper_used"}];
-        }break;
+        }
+            break;
         case TEST_STATUS_PAPER_OUT: {
             [self sendEventWithName:@"gluco_device_connection" body:@{@"status":@"paper_out"}];
-        }break;
+        }
+            break;
         case TEST_STATUS_START_TEST: {
             NSInteger progress = [[params valueForKey:@"test_progress"] integerValue];
             NSInteger countDown = 10 - progress;
             NSLog(@"count down %ld", (long)countDown);
             [self sendEventWithName:@"gluco_test_progress" body:@{@"progress":[NSNumber numberWithInteger:countDown]}];
-        }break;
+        }
+            break;
         case TEST_STATUS_TEST_COMPLETE: {
             
             double result = [[params valueForKey:@"test_value"] doubleValue];
@@ -128,14 +136,17 @@ RCT_EXPORT_METHOD(stopMeasuring) {
             double sugarLevel = result * 16;
             NSLog(@"title %@ sugar %f", title, sugarLevel);
             [self sendEventWithName:@"gluco_test_result" body:@{@"result":[NSNumber numberWithDouble:sugarLevel], @"title":title}];
-        }break;
+        }
+            break;
         case TEST_STATUS_CHECK_ERROR: {
             [self sendEventWithName:@"gluco_device_connection" body:@{@"status":@"connection_failed"}];
-        }break;
+        }
+            break;
             
         case TEST_STATUS_TIME_OUT_DEVICE_SLEEP: {
             [self sendEventWithName:@"gluco_device_connection" body:@{@"status":@"connection_failed"}];
-        }break;
+        }
+            break;
         case TEST_STATUS_NEED_CALIBRATION:
         case TEST_STATUS_LOW_POWER:
         case TEST_STATUS_TEMPERATURE_LOW_ERROR:
